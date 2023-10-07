6283a05cb19896aab3473cac459bec7d6caa25bd
Tanu Chaurasiya <tanuchaurasiya@Tanus-Air.bbrouter>
2023-10-07 21:50:10 +0530

    bookstore with mysql gorm

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"book_management_system_golang/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
